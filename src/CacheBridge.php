28a7430fb861417f12039d4b9b32ea1db4147d27
@小小只^v^ <littlezov@qq.com>
2021-05-30 06:31:13 +0800

    init

--- src/CacheBridge.php
+++ src/CacheBridge.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 /**
+ * #logic 做事不讲究逻辑，再努力也只是重复犯错
+ * ## 何为相思：不删不聊不打扰，可否具体点：曾爱过。何为遗憾：你来我往皆过客，可否具体点：再无你。.
+ *
  * @version 1.0.0
  * @author @小小只^v^ <littlezov@qq.com>  littlezov@qq.com
  * @contact  littlezov@qq.com
